3aefe2ee45a142deab0aa1b77c86236f24904dc7
Gabo <gsarmiento101874@gmail.com>
2023-07-06 21:14:39 -0500

    dietRouter routes set for testing.

--- api/src/routes/dietRouter.js
+++ api/src/routes/dietRouter.js
@@ -4,4 +4,10 @@ const findAllDiets = require("../controllers/findAllDiets");
 
 const dietRouter = Router();
 
+dietRouter.get("/", (req,res) => {
+    // Envia la lista de dietas.
+    //TODO: conectar al controller.
+    res.send('Estoy en la ruta GET /diets.')
+});
+
 module.exports = dietRouter;
