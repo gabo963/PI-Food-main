796c94384ec74a22ac3c9fa040d2ebb0fe5a830c
gabo963 <gsarmiento101874@gmail.com>
2023-07-14 14:18:33 -0500

    fixed axios query

--- api/src/controllers/findAllDiets.js
+++ api/src/controllers/findAllDiets.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const { Diet } = require("../db");
 
 const { API_KEY } = process.env;
@@ -8,7 +9,7 @@ const findAllDiets = async () => {
     // Retorna el arreglo de todas la dietas
     // Si no existen dietas estas se deben precargar.
     let diets = await Diet.findAll();
-    diets = diets.length == 0 ? createDiets : diets; 
+    diets = diets.length == 0 ? createDiets() : diets; 
     return diets;
 };
 
@@ -17,9 +18,14 @@ const createDiets = async () => {
     // Estas dietas se crean en la BBDD.
     //TODO: Hacer el query a spoonacular & encontrar los tipos de dietas unicos.
     //TODO: Crear los tipos de dietas encontrados en spoonacular en la BBDD.
-    const recetas = await axios.get(`${URL}/random?number=1&api_key=${API_KEY}`)
-    console.log(recetas);
-    const newDiets = [];
+    //let newDiets = ["vegetarian","vegan","glutenFree"];
+    let newDiets = [];
+    const recetas = await axios.get(`${URL}/random?number=1&apiKey=${API_KEY}`)
+    .then((response)=>{
+        console.log(response.data);
+    },(reason)=>{
+        throw Error(`There was an error when extracting the recipes from spoonacular: "${reason}".`);
+    });
     return newDiets;
 };
 
