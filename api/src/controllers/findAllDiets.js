6f2743089277f49967e91d21648a414929103be4
gabo963 <gsarmiento101874@gmail.com>
2023-07-15 11:13:54 -0500

    finished all diets

--- api/src/controllers/findAllDiets.js
+++ api/src/controllers/findAllDiets.js
@@ -1,4 +1,3 @@
-const { response } = require("express");
 const { Diet } = require("../db");
 
 const { API_KEY } = process.env;
@@ -9,24 +8,28 @@ const findAllDiets = async () => {
     // Retorna el arreglo de todas la dietas
     // Si no existen dietas estas se deben precargar.
     let diets = await Diet.findAll();
-    diets = diets.length == 0 ? createDiets() : diets; 
+    diets = diets.length == 0 ? createDiets(null) : diets; 
     return diets;
 };
 
-const createDiets = async () => {
+const createDiets = async ( newDiets ) => {
     // hace una consulta a las recetas de spoonacular y extrae todas las dietas unicas.
     // Estas dietas se crean en la BBDD.
-    //TODO: Hacer el query a spoonacular & encontrar los tipos de dietas unicos.
-    //TODO: Crear los tipos de dietas encontrados en spoonacular en la BBDD.
-    //let newDiets = ["vegetarian","vegan","glutenFree"];
-    let newDiets = [];
-    const recetas = await axios.get(`${URL}/random?number=1&apiKey=${API_KEY}`)
-    .then((response)=>{
-        console.log(response.data);
-    },(reason)=>{
-        throw Error(`There was an error when extracting the recipes from spoonacular: "${reason}".`);
-    });
-    return newDiets;
+    if( !newDiets ) {
+        newDiets = ["vegetarian","vegan","glutenFree"];
+        const recetas = await axios.get(`${URL}/random?number=10&apiKey=${API_KEY}`)
+        .then((response)=>{
+            response.data.recipes.forEach(receta => {
+                newDiets = newDiets.concat(receta.diets);
+            });
+        },(reason)=>{
+            throw Error(`There was an error when extracting the recipes from spoonacular: "${reason}".`);
+        });
+    }
+    newDiets = [...new Set(newDiets)];
+    newDiets = newDiets.map( nombre => { return {name: nombre}; });
+    const respuesta = await Diet.bulkCreate(newDiets);
+    return respuesta;
 };
 
 module.exports = findAllDiets;
