c33444d2de539e05d7b64d1242aab30416178c2c
Gabo <gsarmiento101874@gmail.com>
2023-07-18 17:11:29 -0500

    fixed dietRouter imports

--- api/src/routes/dietRouter.js
+++ api/src/routes/dietRouter.js
@@ -1,6 +1,6 @@
 const {Router} = require("express");
 
-const findAllDiets = require("../controllers/findAllDiets");
+const {findAllDiets} = require("../controllers/findAllDiets");
 
 const dietRouter = Router();
 
