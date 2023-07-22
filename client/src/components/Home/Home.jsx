b2e65bc856fbf59c41e0e1b7752d916ca7bdb619
Gabo <gsarmiento101874@gmail.com>
2023-07-22 15:29:03 -0500

    starting home

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,32 @@
+import './Home.css';
+
+import RecipeCard from '../RecipeCard/RecipeCard';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getRecipes } from "../../redux/actions";
+
+
+const Home = () => {
+
+    const recipes = useSelector( (state) => state.recipes );
+    const dispatch = useDispatch();
+
+    useEffect( () => {
+        dispatch( getRecipes() );
+    }, [] );
+
+    return(
+        <div>
+            <h3>Recipes:</h3>
+
+            <div>
+                {recipes.map(
+                    recipe => { return <RecipeCard key={recipe.ID} id={recipe.ID} name={recipe.name} image={recipe.image} /> }
+                )}
+            </div>
+
+        </div>
+    );
+};
+
+export default Home;
