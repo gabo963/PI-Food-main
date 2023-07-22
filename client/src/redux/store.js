836c03be4b870a022a76794a06a775984bb24bf2
Gabo <gsarmiento101874@gmail.com>
2023-07-22 13:26:28 -0500

    added redux-thunk

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,8 +4,7 @@ import thunk from 'redux-thunk';
 
 const composeEnhancers =
    (typeof window !== 'undefined' &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-   compose;
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const store = createStore(
    rootReducer,
