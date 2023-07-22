07ac5e55c46d288493b44bc758131bfd242f6ffd
Gabo <gsarmiento101874@gmail.com>
2023-07-22 15:01:12 -0500

    added new files for routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import {Routes, Route} from "react-router-dom";
 import RecipeCards from './components/RecipeCards';
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Food</h1>
-      <div>
-        <RecipeCards />
-      </div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/celulares/:id" element={<CelularDetail />}/>
+        <Route path="/celulares/create" element={<CreateCelular />}/>
+      </Routes>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import store from './redux/store'
 
 ReactDOM.render(
   <Provider store={store}>
-    {/* Browser Router va adentro */}
-    <App />
-    {/* Browser Router va adentro */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
