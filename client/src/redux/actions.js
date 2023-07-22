d647e11cb9405a09ade6364539ef516eca33d479
Gabo <gsarmiento101874@gmail.com>
2023-07-22 13:49:00 -0500

    added axios to client and included actions

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,5 +1,12 @@
-export const GET_RECIPES = "GET_RECIPES";
+import axios from 'axios';
+export const GET_RECIPES = "GET_RECIPES"; // TODO: puede venir con query
+export const GET_RECIPE = "GET_RECIPE"; // TODO: Enviar el internal flag
+export const GET_DIETS = "GET_DIETS"; // TODO: recibir las dietas.
+export const POST_RECIPE = "POST_RECIPE"; // TODOL: Postear la receta.
 
 export const getRecipes = () => {
-    return { type: GET_RECIPES, payload:[] };
+    return function(dispatch){
+        axios("http://localhost:3001/recipes")
+        .then(data=>dispatch( {type: GET_RECIPES, payload: data.data} ))
+    };
 };
