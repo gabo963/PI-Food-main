e8a04f22c817881f3439de847d3a35ab96b38c44
Gabo <gsarmiento101874@gmail.com>
2023-07-22 13:53:59 -0500

    added reducer cases

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,13 +1,21 @@
-import { GET_RECIPES } from "./actions";
+import { GET_RECIPES, GET_RECIPE, GET_DIETS, POST_RECIPE } from "./actions";
 
 const initialState = {
     recipes: [],
+    diets: [],
+    recipe: null,
 };
 
 const rootReducer = (state=initialState, action) => {
     switch( action.type ) {
         case GET_RECIPES:
-            return {...state, recipes: action.payload }
+            return {...state, recipes: action.payload };
+        case GET_RECIPE:
+            return {...state, recipe: action.payload };
+        case GET_DIETS:
+            return {...state, diets: action.payload};
+        case POST_RECIPE:
+            return {...state, recipes: [...state.recipes, action.payload] };
         default: 
             return {...state};
     }
