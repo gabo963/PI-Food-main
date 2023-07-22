e332063f1efc262c1815535ce74b25a3402ae3ad
Gabo <gsarmiento101874@gmail.com>
2023-07-22 13:06:42 -0500

    added actions

---
+++ client/src/redux/actions.js
@@ -0,0 +1,5 @@
+export const GET_RECIPES = "GET_RECIPES";
+
+export const getRecipes = () => {
+    return { type: GET_RECIPES, payload:[] };
+};
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,9 +1,13 @@
+import { GET_RECIPES } from "./actions";
+
 const initialState = {
     recipes: [{id: 1, name: 'Helado de Vainilla'}],
 };
 
 const rootReducer = (state=initialState, action) => {
     switch( action.type ) {
+        case GET_RECIPES:
+            return {...state, recipes: action.payload }
         default: 
             return {...state};
     }
